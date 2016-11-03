fdd4c89c34e0c7241a10368871c9bd5bc9d566f0
ashley teoh <ashleytqy@gmail.com>
2016-11-02 21:05:59 -0400

    connected to firebase

--- mongo.js
+++ mongo.js
@@ -16,12 +16,6 @@ var bot = new TelegramBot(token, {polling: true});
 bot.onText(/\/start/, function(msg, match) {
   var chatID = msg.chat.id;
   var name = msg.chat.first_name;
-  console.log("chat ID is " + chatID);
-  //initializing an empty list (this is a problem if /start is called amany times!!!)
-  var user = listCollection.child("chatIDs");
-  var obj = {};
-  obj[chatID] = [];
-  user.set(obj);
 
   if (name == undefined) {
     name = "all";
@@ -29,7 +23,8 @@ bot.onText(/\/start/, function(msg, match) {
 
   var output = "hey " + name + "! this is a list bot, of sorts. \
   to use this list bot, type /add [item], /delete [index of item], \
-  /clear, /changeListName [new list name], /peek";
+  /clear, /peek. \n\n please do not store important information in \
+  the lists -- data is not encrypted.";
   bot.sendMessage(chatID, output);
 });
 
@@ -39,11 +34,9 @@ bot.onText(/\/add/, function(msg, match) {
   //getting the input item from user
   var item = match["input"];
   item = item.substring(5);
-  console.log("Item to be added " + item);
 
-  var list = db.ref("server/data/chatIDs/" + chatID);
+  var list = db.ref("server/data/chatIDs/" + chatID + "/list");
   list.once("value", function(snapshot) {
-    console.log("calling...");
     var originalList = snapshot.val();
     //in case it hasn't been initialized yet
     if (originalList == null) {
@@ -53,60 +46,96 @@ bot.onText(/\/add/, function(msg, match) {
     list.set(originalList);
     printListInChat(chatID);
   });
-
 });
 
 
 //
-// bot.onText(/\/delete/, function(msg, match) {
-//   chatID = msg.chat.id;
-//   var entry = Number(msg.text.substring(7));
-//   if (entry > -1 && entry <= list.length) {
-//     //offset zero-based index
-//     list.splice(entry - 1, 1);
-//   }
-//
-//   bot.sendMessage(chatID, listToString());
-// })
-//
-// bot.onText(/\/clear/, function(msg, match) {
-//     chatID = msg.chat.id;
-//     list = [];
-//     bot.sendMessage(chatID, listName + " list emptied!");
-// });
-//
-bot.onText(/\/peek/, function(msg, match) {
+bot.onText(/\/delete/, function(msg, match) {
   chatID = msg.chat.id;
+  var entry = Number(msg.text.substring(7));
+  console.log(entry);
+
+
+  if (isNaN(entry)) {
+    bot.sendMessage(chatID, "Please enter a valid index!");
+  } else {
+      var list = db.ref("server/data/chatIDs/" + chatID + "/list");
+      list.once("value", function(snapshot) {
+
+        var originalList = snapshot.val();
+        //in case it hasn't been initialized yet
+        if (originalList == null) {
+          bot.sendMessage(chatID, "List empty now; nothing to delete!");
+        } else {
+          var length = originalList.length;
+          if (entry > -1 && entry <= length) {
+            //offset zero-based index
+            originalList.splice(entry - 1, 1);
+            console.log(originalList);
+            list.set(originalList);
+            printListInChat(chatID);
+          } else {
+            bot.sendMessage(chatID, "Please enter a valid index!");
+          }
+        }
+      });
+  }
+})
+
+bot.onText(/\/clear/, function(msg, match) {
+    var chatID = msg.chat.id;
+    var list = db.ref("server/data/chatIDs/" + chatID + "/list");
+    list.set([]);
+    bot.sendMessage(chatID, "List emptied!");
+});
+
+
+bot.onText(/\/peek/, function(msg, match) {
+  var chatID = msg.chat.id;
   printListInChat(chatID);
 });
-//
+
 // bot.onText(/\/changeListName/, function(msg, match) {
-//   listName = msg.text.substring(16);
+//   var chatID = msg.chat.id;
+//   var input = msg.text.substring(16);
+//   setListName(input, chatID);
+//   bot.sendMessage(chatID, "List name changed to " + input);
 // })
-//
-//
+
+
 //HELPER FUNCTIONS
 function printListInChat(userChatID) {
-  var list = db.ref("server/data/chatIDs/" + userChatID);
+  var list = db.ref("server/data/chatIDs/" + userChatID + "/list");
   // Attach an asynchronous callback to read the data at our posts reference
-  list.on("value", function(snapshot) {
+  list.once("value", function(snapshot) {
     var output = "";
     var data = snapshot.val();
-    if (data.length != null) {
-      output += "\n list\n";
-      output += "=======\n";
+    if (data != null) {
+      output += "\n" + "* ~ List ~ *" + "\n";
+      output += "========\n";
       for (var i = 0; i < data.length; i ++) {
         output += (i+1) + ". " + data[i] + "\n";
       };
+      bot.sendMessage(userChatID, output);
+    } else {
+      bot.sendMessage(userChatID, "List is empty!");
     }
-
-
-    bot.sendMessage(userChatID, output);
   });
 }
 
-function print(){
-  for (var i = 0; i < list.length; i++) {
-    console.log(idToList[chatID][i]);
-  }
+//these 2 functions dont work right now
+function setListName(name, userChatID) {
+  var listName = db.ref("server/data/chatIDs/" + userChatID + "/listName");
+  listName.once("value", function(data) {
+      listName.set(name);
+  });
 }
+
+function getListName(userChatID){
+  var listName = db.ref("server/data/chatIDs/" + userChatID + "/listName");
+  var string = "";
+  listName.once("value").then(function(data) {
+    string = data.val();
+    return string;;
+  });
+};
